81e85645b0e5939838b803a03b14a5864bc10b40
glaukiol1 <glaukiol1@gmail.com>
2022-02-09 19:44:12 +0100

    online banking changes

--- online_banking/public/js/index.js
+++ online_banking/public/js/index.js
@@ -1,6 +1,19 @@
 
 console.log("Hello From JavaScript File!")
 
+function clearScreen() {
+    document.getElementById('main').innerHTML = ''
+}
+function updateScreen(data) {
+    document.getElementById('main').innerHTML = data
+}
+
+function loggedInScreen(data) {
+    return `
+    <h2>Hello ${data.name}</h2>
+    <h3>Available Funds: <br>EUR€${data.addresses[0].balance} ${data.addresses[0].address} <br> USD$${data.addresses[1].balance} ${data.addresses[1].address}</h3>
+    `
+}
 document.getElementById('submit').addEventListener('click',(ev) => {
     const docidnr = document.getElementById('docidnr')
     const password = document.getElementById('password')
@@ -8,7 +21,8 @@ document.getElementById('submit').addEventListener('click',(ev) => {
     fetch('http://localhost:8080/getuserdata?documentid='+docidnr.value+"&password="+password.value).then(resp=>{
         if (resp.status == 200) {
             resp.json().then(json=>{
-                alert(`Hello, ${json.name}`)
+                clearScreen()
+                updateScreen(loggedInScreen(json))
             })
         } else {
             resp.json().then(json=>{
